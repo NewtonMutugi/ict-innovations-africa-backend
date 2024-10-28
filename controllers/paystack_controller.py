d3f5f75dd97557817618dfdc193aa70657f2fdad
NewtonMutugi <newtonmut197@gmail.com>
2024-10-29 00:24:25 +0300

    Update payment amount in Paystack controller to 39 USD

--- controllers/paystack_controller.py
+++ controllers/paystack_controller.py
@@ -7,7 +7,7 @@ from api.paystack_api import paystack_api
 router = APIRouter()
 
 # Constants
-PAYMENT_AMOUNT = 500000  # 5000 KSH
+PAYMENT_AMOUNT = 3900  # 39 USD
 
 # Dependency for database session
 
