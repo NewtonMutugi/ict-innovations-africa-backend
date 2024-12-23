dba7ab6633f303802caecf06dabe34be21f6c5f6
NewtonMutugi <newtonmut197@gmail.com>
2024-12-23 20:14:17 +0300

    Add utility functions for password hashing and JWT token creation

---
+++ auth/dependancies.py
@@ -0,0 +1,70 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt import PyJWTError as JWTError, PyJWT as jwt
+from sqlalchemy.orm import Session
+from auth.utils import ALGORITHM, SECRET_KEY, verify_password
+from models.auth_model import TokenData
+from database.schema import User
+from database.database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bind=engine)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_user(db: Session, username: str = None, email: str = None):
+    if username:
+        return db.query(User).filter(User.username == username).first()
+    if email:
+        return db.query(User).filter(User.email == email).first()
+    return None
+
+
+def authenticate_user(db: Session, password: str, username: str = None, email: str = None):
+    user = None
+    if username:
+        user = get_user(db, username=username)
+    elif email:
+        user = get_user(db, email=email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+def check_if_username_is_email(username):
+    try:
+        if "@" in username:
+            return True
+        return False
+    except Exception as e:
+        return False
