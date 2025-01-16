9c4eb1f167f80e2e0203c0c286b5cacbed16167c
NewtonMutugi <newtonmut197@gmail.com>
2025-01-16 15:49:20 +0300

    Added hosting plans router to the main API routes

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from controllers.paystack_controller import router as paystack_router
 from controllers.mail_controller import router as mail_router
 from controllers.events_controller import router as event_router
 from controllers.auth_controller import router as auth_router
+from controllers.hosting_plans_controller import router as hosting_plans_router
 
 router = APIRouter()
 
@@ -10,3 +11,4 @@ router.include_router(paystack_router, prefix="/paystack", tags=["paystack"])
 router.include_router(mail_router, prefix="/mail", tags=["mail"])
 router.include_router(event_router, tags=["event"])
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
+router.include_router(hosting_plans_router, tags=["hosting plans"])
