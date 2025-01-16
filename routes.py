f2ea9ca245c2276d895399557955fc31368e726a
NewtonMutugi <newtonmut197@gmail.com>
2025-01-16 15:59:29 +0300

    Add hosting payments router to main API routes

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ from controllers.mail_controller import router as mail_router
 from controllers.events_controller import router as event_router
 from controllers.auth_controller import router as auth_router
 from controllers.hosting_plans_controller import router as hosting_plans_router
+from controllers.hosting_payment_controller import router as hosting_payments_router
 
 router = APIRouter()
 
@@ -12,3 +13,4 @@ router.include_router(mail_router, prefix="/mail", tags=["mail"])
 router.include_router(event_router, tags=["event"])
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
 router.include_router(hosting_plans_router, tags=["hosting plans"])
+router.include_router(hosting_payments_router, tags=["hosting payments"])
